a7a8828764fda04991d426c5ee5b6691f31174ea
geliebterfuehrer <geliebterfuehrer@lavabit.com>
2013-05-07 17:39:03 +0200

    menu_state wert, der für eine Anteige Verwendet wird, wenn er 1 ist, d.h. im Hauptanzeigebildschirm

--- Wecker.c
+++ Wecker.c
@@ -14,6 +14,7 @@ unsigned int _base_y[2] = {0x80, 0xc0};   // example for 2x16 display
             
 unsigned int _lcd_x,_lcd_y,_lcd_maxx;
 unsigned char datetime[6]; // 0. Sekunden, 1. Minute, 2. Stunden, 3. Tage, 4. Monate, 5. Jahre
+unsigned char menu_state = 1
 
 #define LCD_DATA_OUT              P1OUT
 #define LCD_DATA_DIR              P1DIR
@@ -111,138 +112,131 @@ void lcd_init(void){
 		_CommandToLCD(initmuster[i]);      // function set (Interface 8bit) 
 	};
 };
+
+void lcd_gotoxy(unsigned char x, unsigned char y){
 //******************************************************************************
 // Description  : set the LCD display position x=0..39 y=0..3
 // Input        : x - X-position
 //              : y - Y-position
 // Output       : none
 //******************************************************************************
-void lcd_gotoxy(unsigned char x, unsigned char y)
-  {if (y>1) 
-           y=0;
-   _CommandToLCD(_base_y[y]+x); // function set + 8bit bus
-   _lcd_x=x;
-   _lcd_y=y;
-  };
+	if (y>1) y=0;
+	_CommandToLCD(_base_y[y]+x); // function set + 8bit bus
+	_lcd_x=x;
+	_lcd_y=y;
+};
+
+
+void lcd_clear(void){
 //******************************************************************************
 // Description  : clears the LCD
 // Input        : none
 // Output       : none
 //******************************************************************************
-void lcd_clear(void)
-  {
-   _CommandToLCD(0xc1); // cursor off
-                    // function display clear
+	_CommandToLCD(0xc1);	 	// cursor off
+		                    	// function display clear
+	_lcd_x=_lcd_y=0;
+};
 
-   _lcd_x=_lcd_y=0;
-  };
+void lcd_putchar(unsigned char c){
 //******************************************************************************
 // Description  : write a char to the LCD
 // Input        : c - character
 // Output       : none
 //******************************************************************************
-void lcd_putchar(unsigned char c)
-  {
-  
-     if (_lcd_x>=16)
-                          {
-                          _lcd_y++;
-                          lcd_gotoxy(0,_lcd_y);
-                          };
-     _DataToLCD(c);            // send data
-     _lcd_x++;
-  };
+	if (_lcd_x>=16){
+		_lcd_y++;
+		lcd_gotoxy(0,_lcd_y);
+	};
+	_DataToLCD(c);            // send data
+	_lcd_x++;
+};
+
+void lcd_puts(unsigned char *str){
 //******************************************************************************
 // Description  : write the string str to the LCD
 // Input        : *str  pointer to string
 // Output       : none
 //******************************************************************************
-void lcd_puts(unsigned char *str)
-  {
-     while(*str!=0x0)
-     {
-        lcd_putchar(*str);
-        str++;
-     }
-  }
+	while(*str!=0x0){
+		lcd_putchar(*str);
+		str++;
+	}
+}
+
+void int_to_ascii(unsigned char cmd){
 //******************************************************************************
 //Description   :wandelt int in char gibt das uf display aus entsprechnd der position
 //Input         :int wert
 //Output        :none
 //Restrictions  :maximal 2 stellige int
 //******************************************************************************
-void int_to_ascii(unsigned char cmd){
-  unsigned char zehner = 0x30 + datetime[cmd] / 10;
-  unsigned char einer = 0x30 + datetime[cmd] % 10;
-switch(cmd){
-   case 0:
-       lcd_gotoxy(6,0);
-   break;
-
-   case 1:
-        lcd_gotoxy(3,0);
-   break;
-   case 2:
-        lcd_gotoxy(0,0);
-   break;
-   
-   case 3:
-        lcd_gotoxy(0,1);
-   break;
-   case 4:
-      lcd_gotoxy(3,1);
-  break;
-   case 5:
-      lcd_gotoxy(6,1);
-     break;
-}
+	unsigned char zehner = 0x30 + datetime[cmd] / 10;
+	unsigned char einer = 0x30 + datetime[cmd] % 10;
+	switch(cmd){
+		case 0:
+			lcd_gotoxy(6,0);
+			break;
+		case 1:
+			lcd_gotoxy(3,0);
+			break;
+		case 2:
+			lcd_gotoxy(0,0);
+			break;
+		case 3:
+			lcd_gotoxy(0,1);
+			break;
+		case 4:
+			lcd_gotoxy(3,1);
+			break;
+		case 5:
+			lcd_gotoxy(6,1);
+			break;
+	}
 lcd_putchar(zehner);
 lcd_putchar(einer);
 }
+
+void update_datetime(){
 //******************************************************************************
 //Beschreibung  : Zählt eine Sekunde nach oben
 //input         : none
 //output        : none
 //******************************************************************************
-void update_datetime(){
-  datetime[0] = (datetime[0] + 1) % 60;
-  int_to_ascii(0);
-  if (0 == datetime[0]){
-    datetime[1] = (datetime[1] + 1) % 60;
-    int_to_ascii(1);
-    if (0 == datetime[1]){
-      datetime[2] = (datetime[2] +1 ) % 24;
-      int_to_ascii(2);
-      // Für Tage Monate und Jahre entsprechnd weiter machen
-    }
-  }
-  
-  
+	datetime[0] = (datetime[0] + 1) % 60;
+	if (menu_state) int_to_ascii(0);
+	if (0 == datetime[0]){
+		datetime[1] = (datetime[1] + 1) % 60;
+		if (menu_state) int_to_ascii(1);
+		if (0 == datetime[1]){
+			datetime[2] = (datetime[2] +1 ) % 24;
+			if (menu_state) int_to_ascii(2);
+		      // Für Tage Monate und Jahre entsprechnd weiter machen
+		}
+	}  
 }
+
+void main( void ){
 //******************************************************************************
 // Description  : main program
 // Input        : none
 // Output       : none
 //******************************************************************************
-void main( void )
-
-  {
-    // Stop watchdog timer to prevent time out reset
-    WDTCTL = WDTPW + WDTHOLD;
-    lcd_init();
-    datetime[0] = 32;
-    datetime[1] = 25;
-    datetime[2] = 12;
-    datetime[3] = 7;
-    datetime[4] = 5;
-    datetime[5] = 13;
-    //unsigned char text[32]="Datum:  Zeit:  ";
-    //lcd_puts(text);
-    for(int i=0;i<6;i++){
-      int_to_ascii(i);
-    }
-    while (1) {
-    update_datetime();
-    lcd_delay();
-    }
-  }
+	WDTCTL = WDTPW + WDTHOLD;	// Stop watchdog timer to prevent time out reset
+	lcd_init();
+	datetime[0] = 32;
+	datetime[1] = 25;
+	datetime[2] = 12;
+	datetime[3] = 7;
+	datetime[4] = 5;
+	datetime[5] = 13;
+	//unsigned char text[32]="Datum:  Zeit:  ";
+	//lcd_puts(text);
+	for(int i=0;i<6;i++){
+		int_to_ascii(i);
+	}
+	while (1) {
+		update_datetime();
+		lcd_delay();
+	}
+}
