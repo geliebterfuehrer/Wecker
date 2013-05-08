b15744b7a9e6b80000501b290f824bc2d8675a3d
geliebterfuehrer <geliebterfuehrer@lavabit.com>
2013-05-08 18:14:31 +0200

    Neue Umschaltberechnung für Jahre, Tage usw. Status byte für änderungen Wochentagsanzeige

--- Wecker.c
+++ Wecker.c
@@ -8,10 +8,11 @@
 
 #include <msp430f2272.h>
 
-unsigned char datetime[6]; // 0. Sekunden, 1. Minute, 2. Stunden, 3. Tage, 4. Monate, 5. Jahre
-unsigned char menu_state = 1;
-#define F_CPU 800000
-
+unsigned char datetime[7]; // 0. Sekunden, 1. Minute, 2. Stunden, 3. Tage,4. Wochentag(0=Sonntag, 6=Samstag) 5. Monate, 6. Jahre
+unsigned char state = 0x00; //Statusbyte, 0. Skundenänderung 1. Minutenänderung 2. Stundenänderung 3. Tagesänderung 4. Wochentagsänderung 5. Monatänderung 6. Jahresänderung 7. Aktuelles Menü
+unsigned char days_per_month[12] = {31,31,28,31,30,31,30,31,31,30,31,30}; // Tage pro Monat 0. Dezember...11. November
+char u_week_days[7] = {'S','M','D','M','D','F','S'};
+char l_week_days[7] = {'o','o','i','i','o','r','a'};
 unsigned int LCD_base_y[2] = {0x80, 0xc0};   // example for 2x16 display
 // 1. line 0x00..0x0F + Bit7==0x80
 // 2. line 0x40..0x4F + Bit7==0xc0
@@ -33,6 +34,30 @@ unsigned int LCD_x,LCD_y,LCD_max_x;
 
 
 #define F_CPU 800000		//Takt der CPU in Hz
+
+void calc_feb(void){
+//******************************************************************************
+//Beschreibung	: Berechnet anhand des Jahres die Anzahl der Tage des Februars
+//Input		: none
+//Output	: none
+//******************************************************************************
+	if(0 == datetime[6] % 4){			// Durch 4 Teilbare Jahre Schaltjahr
+		//if(0 == datetime[6] % 100){		// Durch 100 Teilbare Jahre kein Schaltjahr
+		//	if(0 == datetime[6] % 400){	// Durch 400 Teilbare Jahre Schaltjahr
+		//		days_per_month[2] = 29;
+		//	}
+		//	else{
+		//		days_per_month[2] = 28;
+		//	}
+		//}
+		//else{
+		days_per_month[2] = 29;
+		//}	
+	}
+	else{
+		days_per_month[2] = 28;
+	}
+}
 
 void delay_ms(unsigned int time2wait)
 {
@@ -147,156 +172,122 @@ void int_to_ascii(unsigned char cmd){
 //Output		: none
 //Einschränkungen	: maximal 2 stellige int
 //******************************************************************************
-	unsigned char zehner = 0x30 + datetime[cmd] / 10;
-	unsigned char einer = 0x30 + datetime[cmd] % 10;
+	unsigned char zehner;
+	unsigned char einer;
+	if(cmd != 4){
+		zehner = 0x30 + datetime[cmd] / 10;	//Zehner stelle in ASCII
+		einer = 0x30 + datetime[cmd] % 10;	//Einer stelle in ASCII
+	}
+	else{
+		zehner = u_week_days[datetime[4]];	//1. Buchstabe wochentag
+		einer = l_week_days[datetime[4]];	//2. Buchstabe wochentag
+	}
+	//0 1 2 3 4 5 6 7 8 9 A B C D E F
+	//********************************
+	//_ _ _ _ _ _ _ _ S S : M M : S S* 0
+	//				 *
+	//_ _ _ _ _ W T _ T T . M M . J J* 1
+	//********************************
 	switch(cmd){
 		case 0:
-			LCD_write_Pos(6,0);
-			break;
-		case 1:
-			LCD_write_Pos(3,0);
-			break;
+			LCD_write_Pos(14,0);		
+			break;				
+		case 1:					
+			LCD_write_Pos(11,0);		
+			break;				
 		case 2:
-			LCD_write_Pos(0,0);
+			LCD_write_Pos(8,0);
 			break;
 		case 3:
-			LCD_write_Pos(0,1);
+			LCD_write_Pos(8,1);
 			break;
 		case 4:
-			LCD_write_Pos(3,1);
+			LCD_write_Pos(5,1);
 			break;
 		case 5:
-			LCD_write_Pos(6,1);
+			LCD_write_Pos(11,1);
+			break;
+		case 6:
+			LCD_write_Pos(14,1);
 			break;
 	}
-LCD_write_char(zehner);
-LCD_write_char(einer);
+	LCD_write_char(zehner);
+	LCD_write_char(einer);
 }
 
-void update_datetime(){
+void update_datetime(void){
 //******************************************************************************
-//Beschreibung  : Zählt eine Sekunde nach oben
+//Beschreibung  : Zählt eine Sekunde nach oben und aktualisier entsprechend alle anderen Werte von datetime
 //input         : none
 //output        : none
 //******************************************************************************
-	datetime[0] = (datetime[0] + 1) % 60;	//Sekunden nach oben zählen
-	if (menu_state) int_to_ascii(0);	//Anzeigen, wenn im Hauptmenü
-	if (0 == datetime[0]){
-		datetime[1] = (datetime[1] + 1) % 60;	//Minuten hoch zählen
-		if (menu_state) int_to_ascii(1);	//Anzeigen, wenn im Hauptmenü
-		if (0 == datetime[1]){
-			datetime[2] = (datetime[2] + 1) % 24;	//Stunden nach oben zählen
-			if (menu_state) int_to_ascii(2);	//Anzeigen, wenn im Hauptmenü
-			if (0 == datetime[2]){
-				datetime[3]++;			//Tag erhöhen
-				switch(datetime[4]){		//je nach Monat
-					case 1:		//Januar
-						if (32 <= datetime[3]){		//Januar -> 
-							datetime[3] = 1;	// Februar
-							datetime[4] = 2;
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, enn im Hauptmenü
-						}
-						break;
-					case 2:		//Februar
-						if (0 == datetime[5] % 4){	//Schaltjahr
-							if (30 == datetime[3]){		//Januar -> 
-								datetime[3] = 1;	// Februar
-								datetime[4] = 3;
-								if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-							}
-						}
-						else{
-							if (29 == datetime[3]){		//Januar -> 
-								datetime[3] = 1;	// Februar
-								datetime[4] = 3;
-								if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-							}
-						}
-						break;
-					case 3:		//März
-						if (32 == datetime[3]){		//März -> 
-							datetime[3] = 1;	// April
-							datetime[4] = 4;
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 4:		//April
-						if (31 == datetime[3]){
-							datetime[3] = 1;	// April ->
-							datetime[4] = 5;	//Mai
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 5:		//Mai
-						if (32 == datetime[3]){
-							datetime[3] = 1;	// Mai ->
-							datetime[4] = 6;	//Juni
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 6:		//Juni
-						if (31 == datetime[3]){
-							datetime[3] = 1;	// Juni ->
-							datetime[4] = 7;	//Juli
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 7:		//Juli
-						if (32 == datetime[3]){
-							datetime[3] = 1;	// Juli ->
-							datetime[4] = 8;	//August
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 8:		//August
-						if (32 == datetime[3]){
-							datetime[3] = 1;	// August ->
-							datetime[4] = 9;	//September
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 9:		//September
-						if (31 == datetime[3]){
-							datetime[3] = 1;	// September ->
-							datetime[4] = 10;	//Oktober
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 10:	//Oktober
-						if (32 == datetime[3]){
-							datetime[3] = 1;	// Oktober ->
-							datetime[4] = 11;	//November
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 11:	//November
-						if (31 == datetime[3]){
-							datetime[3] = 1;	// November ->
-							datetime[4] = 12;	//Dezember
-							if (menue_state) int_to_ascii(4);	//Monat anzeigen, wenn im Hauptmenü
-						}
-						break;
-					case 12:	//Dezember
-						if (31 == datetime[3]){
-							datetime[3] = 1;	// Dezember ->
-							datetime[4] = 1;	//Januar
-							datetime[5]++;		//Neus Jahr
-							if (menu_state){
-								int_to_ascii(4);
-								int_to_ascii(5);
-							}
-						}
-						break;
-					if (menu_state) int_to_ascii(3);	//Tag Anzeigen, wenn im Hauptmenü
+	datetime[0] = (datetime[0] + 1) % 60;	// Sekunde erhöhen
+	state |= BIT0;				// Statusbit 0 auf 1 gesetzt, veränderung der Sekunde
+	if(0 == datetime[0]){			// Wenn sekunden 0 sind, dann wird Minuten erhöht
+		datetime[1] = (datetime[1] + 1) %60;	// Minute erhöhen
+		state |= BIT1;				// Statusbit 1 auf 1 setzen, veränderung der Minute
+		if(0 == datetime[1]){				// Wenn Minuten 0 sind werden Stunden erhöht
+			datetime[2] = (datetime[2] + 1) % 24;	// Stunden erhöhen
+			state |= BIT2;				// Statusbit 2 auf 1 setzen, veränderung der Stunde
+			if(0 == datetime[2]){				// Wenn Stunden 0 sind wird Tag und Wochentag erhöht
+				datetime[3] = (datetime[3] +1);		// Tag wird um 1 erhöht
+				state |= BIT3;				// Statusbit 3 auf 1 setzen, veränderung Tag
+				datetime[4] = (datetime[4] + 1) % 7; 	// Wochentag wird erhöht
+				state |= BIT4;				// Statusbit 4 auf 1 setzen, veränderung Wochentag
+				if (datetime[3] > days_per_month[datetime[5]]){	// Wenn Tage tage im Monat überschriten,
+					datetime[3] = 1;			// dann Tage auf 1 setzen und
+					datetime[5] = (datetime[5] + 1) % 12;	// Monat um 1 erhöhen
+					state |= BIT5;				// Statusbit 5 auf 1 setzen, veränderung Monat
+					if(1 == datetime[5]){			// Wenn neuer Monat Januer, dann
+						datetime[6]++;			// Jahr um 1 erhöhen
+						state |= BIT6;			// Statusbit 6 auf 1 setzten, jahr verändert
+						calc_feb();			// Februarlänge neu berechnen
+					}
 				}
-				
 			}
 		}
 	}
- 
 }
 
-void main( void ){
+void LCD_update(void){
+//******************************************************************************
+// Beschreibung	: aktualisiert das LC display je nach status des des Menüaufrufs
+// input	: none
+// output	: none
+//******************************************************************************
+	switch(state){
+		case 0xFF:		//Alle Werte haben sich geändert und Hauptmenü
+			for(int i=0;i<7;i++){
+				int_to_ascii(i);
+			}
+			break;
+		case 0xBF:		//Jahr konstant, Hauptmenü
+			for(int i=0;i<6;i++){
+				int_to_ascii(i);
+			}
+			break;
+		case 0x9F:		//Jahr, Monat konstant, HM
+			for(int i=0;i<5;i++){
+				int_to_ascii(i);
+			}
+			break;
+		case 0x87:		//Jahr, Monat, Tag Konstant, HM
+			for(int i=0;i<3;i++){
+				int_to_ascii(i);
+			}
+			break;
+		case 0x83:		//Jahr, Monat, Tag, Stunde konstant , HM
+			for(int i=0;i<2;i++){
+				int_to_ascii(i);
+			}
+			break;
+		case 0x81:		// nur Sekunde hat sich geändert, HM
+			int_to_ascii(1);
+			break;
+	}
+}
+
+int main(void){
 //******************************************************************************
 // Description  : main program
 // Input        : none
@@ -319,4 +310,5 @@ void main( void ){
 		update_datetime();
 		delay_ms(1000);
 	}
+	return 0;
 }
